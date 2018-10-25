e21a7d45e23968eee841bf4699af86aad3c8e6a9
robotgonzalez <44357519+robotgonzalez@users.noreply.github.com>
2018-10-25 21:39:51 +0200

    Update action-snips-test.py

--- action-snips-test.py
+++ action-snips-test.py
@@ -12,6 +12,9 @@ import untangle
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
+latitude = '59.933333'
+longitude = '10.716667'
+
 class SnipsConfigParser(ConfigParser.SafeConfigParser):
     def to_dict(self):
         return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
@@ -25,38 +28,93 @@ def read_configuration_file(configuration_file):
             return conf_parser.to_dict()
     except (IOError, ConfigParser.Error) as e:
         return dict()
+ 
+
+#def numbers_to_strings(argument):
+#    switcher = {
+#        0: "zero",
+#        1: "one",
+#        2: "two",
+#    }
+#    return switcher.get(argument, "nothing")
+
 
 def subscribe_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
     action_wrapper(hermes, intentMessage, conf)
 
 
-def action_wrapper(hermes, intentMessage, conf):
-    #r = requests.get('https://en.wikipedia.org/api/rest_v1/page/summary/Felinae')
-    #data = r.json()
-    #result_sentence = data['extract']
+def precipitation_next_hours():
+    r = requests.get('https://api.met.no/weatherapi/nowcast/0.9/?lat='+latitude+'&lon='+longitude)
+    data = untangle.parse(r.content)
     
-    now = datetime.datetime.now()
-    date = now.strftime("%Y %m %d")
-    #time = now.strftime("%H %M")
+    millimeter = 0
+    times = data.weatherdata.product.time
     
-    result_sentence = date
+    for t in times:
+       millimeter +=  float(t.location.precipitation['value'])
     
-    #print(date)
-    
-    r = requests.get('https://api.met.no/weatherapi/sunrise/1.1/?lat=62.308611&lon=6.937222&date='+now.strftime("%Y-%m-%d"))
-    doc = untangle.parse(r.content)
+    return 'in the next hours it will rain an average of '+str(millimeter / len(times))+' millimeters per hour'
 
-    sun_rise = doc.astrodata.time.location.sun['rise']
+
+def extrem_data_norway():
+    r = requests.get('https://api.met.no/weatherapi/extremeswwc/1.2/')
+    data = untangle.parse(r.content)
+    
+    precipitation_place = data.weatherdata.product.time[0].maximumPrecipitations.location[0]['name']
+    precipitation_value = data.weatherdata.product.time[0].maximumPrecipitations.location[0].maximumPrecipitation['value']
+    
+    temperature_min_place = data.weatherdata.product.time[0].lowestTemperatures.location[0]['name']
+    temperature_min_value = data.weatherdata.product.time[0].lowestTemperatures.location[0].lowestTemperature['value']
+    
+    temperature_max_place = data.weatherdata.product.time[0].highestTemperatures.location[0]['name']
+    temperature_max_value = data.weatherdata.product.time[0].lowestTemperatures.location[0].highestTemperature['value']
+    
+    return 'in norway in the last 24 hours\
+    the most rain fell in '+precipitation_place+' with '+precipitation_value+' millimeters.\
+    the coldest place was '+temperature_min_place+' with '+temperature_min_value+' degree celsius.\
+    the warmest place was '+temperature_max_place+' with '+temperature_max_value+' degree celsius.'
+
+
+def moon_phase():
+    now = datetime.datetime.now()
+    
+    r = requests.get('https://api.met.no/weatherapi/sunrise/1.1/?lat='+latitude+'&lon='+longitude+'&date='+now.strftime("%Y-%m-%d"))
+    data = untangle.parse(r.content)
+    
+    return 'the moon phase today is '+data.astrodata.time.location.moon['phase']
+
+
+def sun_rise_set():
+    now = datetime.datetime.now()
+    
+    r = requests.get('https://api.met.no/weatherapi/sunrise/1.1/?lat='+latitude+'&lon='+longitude+'&date='+now.strftime("%Y-%m-%d"))
+    data = untangle.parse(r.content)
+    
+    sun_rise = data.astrodata.time.location.sun['rise']
     sun_rise = datetime.datetime.strptime(sun_rise[:19], '%Y-%m-%dT%H:%M:%S')
     
-    sun_set = doc.astrodata.time.location.sun['set']
+    sun_set = data.astrodata.time.location.sun['set']
     sun_set = datetime.datetime.strptime(sun_set[:19], '%Y-%m-%dT%H:%M:%S')
     
-    result_sentence = 'the sun rises at '+sun_rise.strftime("%H %M").replace('0', '')+' and sets at '+sun_set.strftime("%H %M").replace('0', '')
+    return 'the sun rises at '+sun_rise.strftime("%H %M").replace('0', '')+' and sets at '+sun_set.strftime("%H %M").replace('0', '')
+
+
+def date_now():
+    date = now.strftime("%Y %m %d")
     
+    return date
+
+
+def wikipedia():
+    r = requests.get('https://en.wikipedia.org/api/rest_v1/page/summary/Felinae')
+    data = r.json()
+    return = data['extract']
+
+
+def action_wrapper(hermes, intentMessage, conf):
     current_session_id = intentMessage.session_id
-    hermes.publish_end_session(current_session_id, result_sentence)
+    hermes.publish_end_session(current_session_id, sun_rise_set())
 
 
 if __name__ == "__main__":
