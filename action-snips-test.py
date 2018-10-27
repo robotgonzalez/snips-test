d97b28f594d83a3f6da766dc9f7178dcc599dc14
snoopy <robotgonzalez@gmail.com>
2018-10-27 21:57:09 +0200

    testing

--- action-snips-test.py
+++ action-snips-test.py
@@ -134,4 +134,4 @@ if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h.subscribe_intent("gonzalez:News", latest_news_nrk) \
             subscribe_intent("gonzalez:Sun", sun_rise_set) \
-         .start()
+         .loop_forever()
