f8c059b9e563ab2601bd8a5d58596688f6d746de
snoopy <robotgonzalez@gmail.com>
2018-10-27 21:54:07 +0200

    testing

--- action-snips-test.py
+++ action-snips-test.py
@@ -130,4 +130,5 @@ def latest_news_nrk(hermes, intentMessage):
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h.subscribe_intent("gonzalez:News", latest_news_nrk) \
+            subscribe_intent("gonzalez:Sun", latest_news_nrk) \
          .start()
